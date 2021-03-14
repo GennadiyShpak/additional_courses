d22a378a2a8e7208af281377e0fa8f81449590eb
Gennadiy <borchuga@gmail.com>
2021-03-14 11:11:47 +0200

    final version

---
+++ FL15_HW7/homework/js/task1.js
@@ -0,0 +1,43 @@
+let deposit =Number(prompt('Enter initial deposit value (initial amount can’t be less than 1000)', 0));
+let durationOfDeposit = Number(prompt('Enter term of deposit number of years can’t be less than 1', 0));
+let percent = Number(prompt('Enter percent value', 0));
+
+
+
+
+const amountCalculator = (deposit, duration,percent) => {
+    const isDepositNan = isNaN(Number(deposit)),
+          isDurationNan = isNaN(Number(duration)),
+          isPercentNan = isNaN(Number(percent)),
+          minValueOfDeposit = 1000,
+          minDuration = 1,
+          maxPercent = 100,
+          numberOfDigitsToRound = 2;
+    let balance = deposit,
+        totalProfit = 0,
+        result;
+    
+    if (isDepositNan || isDurationNan || isPercentNan
+        || deposit < minValueOfDeposit 
+        || durationOfDeposit < minDuration 
+        || percent > maxPercent) {
+          alert('Your input not valid'); 
+         return;
+        }
+        for (let i =0; i<durationOfDeposit; i+=1) {
+            let depositIncome = balance * ( percent/maxPercent );
+            const roundDepositIncome = Number(depositIncome.toFixed(numberOfDigitsToRound))
+            totalProfit = totalProfit + roundDepositIncome;
+            balance += roundDepositIncome;
+            result =Number(balance.toFixed(numberOfDigitsToRound))
+         }
+        alert(`Initial amount: ${deposit}
+    Number of years: ${duration}
+    Percentage of year: ${percent}
+    Total profit: ${totalProfit}
+    Total amount: ${result}`);
+}
+amountCalculator(deposit, durationOfDeposit,percent )
+
+
+
---
+++ FL15_HW7/homework/js/task2.js
@@ -0,0 +1,86 @@
+const two = 2,
+      three = 3;
+
+let maxValue = 8,
+    minValue = 0,
+    firstPrize = 100,
+    secondPrize = 50,
+    thirdPrize = 25,
+    totalPrize = 0,
+    numberOfTryes = 3;
+
+const possibleRewards = () => {
+    if(numberOfTryes === three) {
+        return firstPrize;
+    }
+    if(numberOfTryes === two) {
+        return secondPrize;
+    }
+    if(numberOfTryes === 1) {
+        return thirdPrize;
+    }
+    return
+  }
+const startGame = confirm('Do you want to play a game?');
+     
+function messageGenerator () {
+ const message = parseInt(prompt(`Choose a roulette pocket number from ${minValue} to ${maxValue}
+    Attempts left: ${numberOfTryes}
+    Total prize: ${totalPrize}
+    Possible prize of current attempt: ${possibleRewards()}`))
+    return message;
+}
+
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let currentNumber = randomNumber(minValue, maxValue)
+
+
+
+const resumeGameHandler = () => {
+    firstPrize *= two;
+    secondPrize *= two;
+    thirdPrize *= two;
+    numberOfTryes = three;
+    currentNumber = randomNumber(minValue, maxValue);
+    casino(messageGenerator())
+}
+
+const casino = (num) => {
+    if (isNaN(num) || num === '') {
+        alert('You entered wrong value, and all your money is taken by the casino');
+        return;
+    }
+    
+    if (num !== currentNumber) {
+    numberOfTryes -= 1;
+    if (numberOfTryes === 0) {
+        alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+        return;
+    }
+        possibleRewards()
+        casino(messageGenerator())
+    }
+    if (num === currentNumber) {
+        totalPrize += possibleRewards();
+        const resumeGame = confirm(`Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`)
+        if (!resumeGame) {
+          alert(`Thank you for your participation. Your prize is: ${totalPrize}$`)
+          const resumeGame = confirm(`Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`)
+            if (resumeGame) {
+                resumeGameHandler()
+            }
+            return;
+        }
+        resumeGameHandler()
+    }
+}
+const runGame = () => {
+    if (!startGame) {
+        alert('You did not become a billionaire, but can.')
+        return;
+    } 
+    casino(messageGenerator())
+}
+runGame()
