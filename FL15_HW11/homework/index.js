5840e92dd759374de488508a7bf539e57138fe4e
Gennadiy <borchuga@gmail.com>
2021-03-24 00:56:14 +0200

    final version

---
+++ FL15_HW11/homework/index.js
@@ -0,0 +1,38 @@
+const isEquals = (a,b) => a === b;
+
+const isBigger = (a,b) => a > b;
+
+const storeNames = function () {
+   return [...arguments]
+};
+
+const getDifference = (a, b) => {
+    const result = a > b ? a - b: b - a;
+    return result
+}
+
+const negativeCount = function (arr) {
+    const negativeArr = arr.filter(item => item < 0)
+    return negativeArr.length
+}
+
+const letterCount = function (str1, str2) {
+const transfomStr = str1.split('')
+const filtredArr = transfomStr.filter(item => item === str2)
+return filtredArr.length
+}
+
+const countPoints = function (arr) {
+    let points = 0;
+    const pointsPerWin = 3;
+    arr.forEach(item => {
+        let gameScore = item.split(':');
+        if (Number(gameScore[0]) > Number(gameScore[1])) {
+            points += pointsPerWin; 
+         }
+         if (Number(gameScore[0])=== Number(gameScore[1])) {
+             points +=1
+         }
+    });
+    return points
+}
