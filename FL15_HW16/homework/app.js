2eec12858ba6be7affe511662d73aafad2487818
Gennadiy <borchuga@gmail.com>
2021-04-07 12:02:12 +0300

    temp version

--- FL15_HW16/homework/app.js
+++ FL15_HW16/homework/app.js
@@ -1,6 +1,9 @@
 const root = document.getElementById('root');
+
 const maxTweetLength = 140,
-  timeOutTime = 3000;
+  timeOutTime = 3000,
+  editHashString = 7;
+
 const refs = {
   addMenu: document.getElementById('modifyItem'),
   mainPaige: document.getElementById('tweetItems'),
@@ -26,11 +29,13 @@ const {
 class Twetter {
   constructor() {
     this.allMessage = [];
+    this.counter = 0;
   }
   createTweet(text) {
+    this.counter += 1;
     const tweet = {
       text,
-      id: this.allMessage.length,
+      id: this.counter,
       like: false
     };
     this.allMessage.push(tweet);
@@ -40,27 +45,68 @@ class Twetter {
     return this.allMessage;
   }
 
-  changLike(id) {
-    this.allMessage.forEach((tweet, i) => {
-      if (i === Number(id)) {
-        this.allMessage[i].like = !tweet.like;
+  changeLikeBtn(id) {
+    this.allMessage.forEach(tweet => {
+      if (tweet.id === Number(id)) {
+        tweet.like = !tweet.like;
       }
       return;
     });
   }
 
   isUnique(text) {
-    let result;
+    let result = false;
     this.allMessage.forEach(tweet => {
-      result = Object.values(tweet).includes(text);
-      if (!result) {
-        return;
+      if (Object.values(tweet)[0] === text) {
+        result = true;
       }
     });
     return result;
   }
+  haveLike(id) {
+    let result;
+    this.allMessage.forEach(tweet => {
+      if (tweet.id === Number(id)) {
+        result = tweet.like;
+      }
+    });
+    return result;
+  }
+  removeTweet(id) {
+    const newMessageArr = this.allMessage.filter(tweet => {
+      return tweet.id !== Number(id);
+    });
+    this.allMessage = [...newMessageArr];
+    return;
+  }
+
+  getTweet(id) {
+    let handleTweet;
+    this.allMessage.forEach(tweet => {
+      if (tweet.id === Number(id)) {
+        handleTweet = tweet;
+      }
+    });
+    return handleTweet;
+  }
+
+  // editTweet(tweet) {
+  //   this.allMessage.forEach(item => {
+  //     if (tweet.id === item.id) {
+  //       item.text = tweet.text;
+  //     }
+  //   });
+  // }
+  editTweet(id, text) {
+    this.allMessage.forEach(item => {
+      if (Number(id) === item.id) {
+        item.text = text;
+      }
+    });
+  }
 }
 
+twitArea.classList.add('twitArea');
 tweettsList.classList.add('tweettList');
 const myTwitter = new Twetter();
 
@@ -76,6 +122,7 @@ const addBtnHandler = () => {
   // mainPaige.classList.add('hidden');
   addMenu.classList.remove('hidden');
   addMenu.classList.add('addMenuStyle');
+  location.hash = '#/add';
 };
 
 const marckup = obj => {
@@ -84,24 +131,61 @@ const marckup = obj => {
     return `<li class="tweet__wrapper" id="${id}"><p>${text}</p>
     <div>
     <button type="button" id="removeBtn" class="remove-btn btn removeBtn" data-id=${id}>remove</button>
-    <button type="button" class="dislike-btn btn likeBtn" data-id=${id}>like</button></li>
+    <button type="button" class="dislike-btn btn likeBtn" data-id=${id}>dislike</button></li>
     </div>
     `;
   } else {
     return `<li class="tweet__wrapper" id="${id}"><p>${text}</p>
     <div>
     <button type="button" id="removeBtn" class="remove-btn btn removeBtn" data-id=${id}>remove</button>
-    <button type="button" class="dislike-btn btn likeBtn" data-id=${id} >like</button></li>
+    <button type="button" class="dislike-btn btn likeBtn" data-id=${id}>like</button></li>
     </div>
     `;
   }
 };
 
-const createTweetHandler = () => {
+const renameLikeButton = btn => {
+  myTwitter.changeLikeBtn(btn.dataset.id);
+  if (myTwitter.haveLike(btn.dataset.id)) {
+    btn.textContent = 'dislike';
+    return;
+  }
+  btn.textContent = 'like';
+};
+const editTweet = (e, el) => {
+  const { target } = e;
+  if (target.nodeName === 'LI') {
+    location.hash = `#/edit/${el.id}`;
+    const editingTweet = myTwitter.getTweet(el.id);
+    twitArea.value = editingTweet.text;
+  }
+};
+
+const deleteBtn = btn => {
+  myTwitter.removeTweet(btn.dataset.id);
+  tweettsList.innerHTML = '';
+  myTwitter.allMessage.forEach(tweet => {
+    tweettsList.insertAdjacentHTML('beforeend', marckup(tweet));
+  });
+  const likeBtns = document.querySelectorAll('.likeBtn');
+  const removeBtns = document.querySelectorAll('.removeBtn');
+  [...likeBtns].forEach(btn =>
+    btn.addEventListener('click', () => {
+      renameLikeButton(btn);
+    })
+  );
+  [...removeBtns].forEach(btn => {
+    btn.addEventListener('click', () => {
+      deleteBtn(btn);
+    });
+  });
+};
+
+const createTweetHandler = text => {
   if (twitArea.value.length === 0) {
     return;
   }
-  if (myTwitter.isUnique(twitArea.value)) {
+  if (myTwitter.isUnique(text)) {
     notValidTweettError("Error! You can't tweet about that");
     return;
   }
@@ -111,11 +195,26 @@ const createTweetHandler = () => {
   } else {
     myTwitter.createTweet(twitArea.value);
     twitArea.value = '';
-    marckupHandler(myTwitter.allMessage);
-    const removeBtn = document.querySelectorAll('.likeBtn');
-    [...removeBtn].forEach(btn =>
+    localStorage.setItem('myTwetter', JSON.stringify(myTwitter.allMessage));
+    const lSHandler = JSON.parse(localStorage.getItem('myTwetter'));
+    marckupHandler(lSHandler);
+
+    const likeBtns = document.querySelectorAll('.likeBtn');
+    const removeBtns = document.querySelectorAll('.removeBtn');
+    const tweetItem = document.querySelectorAll('.tweet__wrapper');
+    [...likeBtns].forEach(btn =>
       btn.addEventListener('click', () => {
-        myTwitter.changLike(btn.dataset.id);
+        renameLikeButton(btn);
+      })
+    );
+    [...removeBtns].forEach(btn => {
+      btn.addEventListener('click', () => {
+        deleteBtn(btn);
+      });
+    });
+    [...tweetItem].forEach(tweet =>
+      tweet.addEventListener('click', e => {
+        editTweet(e, tweet);
       })
     );
   }
@@ -128,5 +227,16 @@ const marckupHandler = arr => {
   });
 };
 
+const saveChangesHandler = () => {
+  const createHash = /^[#/add]+$/;
+  if (createHash.test(location.hash)) {
+    createTweetHandler(twitArea.value);
+    return;
+  }
+  const editHash = location.hash.slice(editHashString);
+  myTwitter.editTweet(editHash, twitArea.value);
+  marckupHandler(myTwitter.allMessage);
+};
+
 addBtn.addEventListener('click', addBtnHandler);
-saveChangesInInput.addEventListener('click', createTweetHandler);
+saveChangesInInput.addEventListener('click', saveChangesHandler);
