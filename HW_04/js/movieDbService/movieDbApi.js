07f4b54178e28c89478572a3d1609cd364c79d76
Hennadii Shpak <borchuga@gmail.com>
2021-05-22 22:32:31 +0300

    initial commit

---
+++ HW_04/js/movieDbService/movieDbApi.js
@@ -0,0 +1,35 @@
+const API_KEY = 'bb3f2a9bd6a374d8a5257ae7f0ad6ee7';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const trendsMovieByWeek = 'trending/movie/week';
+
+const getTrends = async () => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}${trendsMovieByWeek}?api_key=${API_KEY}`,
+    );
+    const { results } = await response.json();
+    console.log('results', results[0]);
+    return results;
+  } catch (error) {
+    console.log('error', { error });
+  }
+};
+
+const getInfo = async (id, category) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}movie/${id}/${category}?api_key=${API_KEY}`,
+    );
+    const { results } = await response.json();
+    // console.log('results', results);
+    return results;
+  } catch (error) {
+    console.log('error', { error });
+  }
+};
+const movieApi = {
+  getTrends,
+  getInfo,
+};
+
+export default movieApi;
